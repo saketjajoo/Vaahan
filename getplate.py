ce9fd300098136cd9f91a5332fc872585695d90d
Saket Jajoo <saketjajoo77@gmail.com>
2019-12-11 23:00:18 +0530

    Update getplate.py

--- getplate.py
+++ getplate.py
@@ -7,7 +7,7 @@ def get_vno(img):
         response = requests.post(
             'https://api.platerecognizer.com/v1/plate-reader/',
             files=dict(upload=fp),
-            headers={'Authorization': 'Token 679d2b840e1a45d255896209ba8185389a2d7a5f'})
+            headers={'Authorization': 'Token YOUR_API_TOKEN'})
     json_data = response.json()
     parsed_json = (json.dumps(json_data))
     o = json.loads(parsed_json)
